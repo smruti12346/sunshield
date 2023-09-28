122bef5c9412325604b53e036f0ee1846272e988
sonali samantaray <sonalisamantaray.thetechnovate@gmail.com>
2023-09-28 17:53:46 +0530

    28/9/2023

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,20 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head >
-     
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-KMEQRXRY2B"/>
+      <script dangerouslySetInnerHTML={{
+        __html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        
+         gtag('config', 'G-KMEQRXRY2B');
+        `
+       }}/>
+      
+      <meta name="google-site-verification" content="G4JkJJc4_Xvqoqq7ASsdjKN8emO24n6TcfTCi4wsLKw" />
+      
+      
       </Head>
       <body>
         <Main />
