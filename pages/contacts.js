a85f2476fa1a49b4a5b5d23ee14981b34406cd09
Himansu <himansu.thetechnovate@gmail.com>
2023-10-02 19:47:47 +0530

    02/10

--- pages/contacts.js
+++ pages/contacts.js
@@ -46,7 +46,7 @@ const Contact = () => {
         try {
             setIsLoading(true); // Set loading state to true
             const response = await axios.post(
-                'https://api.sunshieldexim.com/wp-json/contact-form-7/v1/contact-forms/163/feedback',
+                'https://api.sunshieldexim.com/wp-json/contact-form-7/v1/contact-forms/12/feedback',
                 values,
                 {
                     headers: {
